36e36882e4aa72b3377d025a9de944be59d6ede9
cwon222 <qoxowjd77@naver.com>
2024-03-17 19:42:33 +0900

    0317

---
+++ Assets/IdleState.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : StateMachineBehaviour
+{
+    readonly int isPatrolling_Hash = Animator.StringToHash("IsPatrolling");
+    float timer;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer = 0.0f;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            animator.SetBool(isPatrolling_Hash, true);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
