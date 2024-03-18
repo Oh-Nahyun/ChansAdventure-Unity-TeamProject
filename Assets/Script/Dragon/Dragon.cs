988847b0fb4749c9dea8f8b25385ffd01e2121d3
cwon222 <qoxowjd77@naver.com>
2024-03-18 21:22:08 +0900

    드래곤 데미지 입기
    드래곤이 피격 받으면 경직 애니메이션 실행 죽으면 죽는 애니메이션 실행

---
+++ Assets/Script/Dragon/Dragon.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    public float HP = 100.0f;
+
+    readonly int die_Hash = Animator.StringToHash("Die");
+    readonly int damage_Hash = Animator.StringToHash("Damage");
+
+    Animator animator;
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        HP -= damageAmount;
+        if(HP <= 0)
+        {
+            // 죽는 애니메이션
+            animator.SetTrigger(die_Hash);
+            GetComponent<Collider>().enabled = false;
+        }
+        else
+        {
+            // 맞는 애니메이션
+            animator.SetTrigger(damage_Hash);
+        }
+    }
+}
