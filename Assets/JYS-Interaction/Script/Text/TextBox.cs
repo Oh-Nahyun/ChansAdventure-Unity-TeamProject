78b7a2f6a0d73d1b0d2d70f987cf979999985a3b
uk <134572636+bide879@users.noreply.github.com>
2024-03-20 15:20:19 +0900

    타이핑 종료 시 버튼, 선택지 버튼 추가
    타이핑 종료 시 버튼 완료, 선택지 버튼 시작

--- Assets/JYS-Interaction/Script/Text/TextBox.cs
+++ Assets/JYS-Interaction/Script/Text/TextBox.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
@@ -14,7 +15,10 @@ public class TextBox : MonoBehaviour
     TextMeshProUGUI talkText;
     TextMeshProUGUI nameText;
     CanvasGroup canvasGroup;
+    Image endImage;
     public GameObject scanObject;
+
+    TextSelet textSelet;
 
     public string talkString;
     public int talkIndex = 0;
@@ -33,12 +37,19 @@ public class TextBox : MonoBehaviour
     private void Awake()
     {
         canvasGroup = GetComponent<CanvasGroup>();
+
         Transform child = transform.GetChild(0);
-
         talkText = child.GetComponent<TextMeshProUGUI>();
-        child = transform.GetChild(1);
 
+        child = transform.GetChild(1);
         nameText = child.GetComponent<TextMeshProUGUI>();
+
+        child = transform.GetChild(2);
+        endImage = child.GetComponent<Image>();
+
+        child = transform.GetChild(3);
+        textSelet = child.GetComponent<TextSelet>();
+
         talkData = new Dictionary<int, string[]>();
 
         StopAllCoroutines();
@@ -50,8 +61,9 @@ public class TextBox : MonoBehaviour
         canvasGroup.alpha = 0.0f;
         canvasGroup.interactable = false;
         canvasGroup.blocksRaycasts = false;
-
         
+        
+
         GameManager.Instance.onTalkNPC += () =>
         {
             //isNpc = true;
@@ -63,7 +75,6 @@ public class TextBox : MonoBehaviour
             //isNpc = false;
             ObjAction();
         };
-
     }
 
     public void Action()
@@ -73,6 +84,7 @@ public class TextBox : MonoBehaviour
         //scanObject = gameObject;
         if (typingTalk == false)
         {
+            endImage.color = new Color(endImage.color.r, endImage.color.g, endImage.color.b, 0f);
             StartCoroutine(TalkStart());
         }
         else
@@ -87,6 +99,7 @@ public class TextBox : MonoBehaviour
             Talk(NPCdata.id);
             nameText.text = $"{NPCdata.nameNPC}";
             //talkText.text = $"{talkString}";
+            endImage.color = new Color(endImage.color.r, endImage.color.g, endImage.color.b, 1f);
             typingTalk = false;
         }
     }
@@ -183,6 +196,7 @@ public class TextBox : MonoBehaviour
             yield return new WaitForSeconds(charPerSeconds);
             if (i + 2 > text.Length)
             {
+
                 typingTalk = false;
             }
         }
@@ -208,7 +222,31 @@ public class TextBox : MonoBehaviour
         talkData.Add(110, new string[] { "이미 아이템을 획득한 상자이다." });
         talkData.Add(1000, new string[] { "애국가는 말 그대로 '나라를 사랑하는 노래'를 뜻한다.", "1896년 '독립신문' 창간을 계기로 여러 가지의 애국가 가사가 신문에 게재되기 시작했는데", "이 노래들을 어떤 곡조로 불렀는가는 명확하지 않다.", "다만 대한제국이 서구식 군악대를 조직해 1902년 '대한제국 애국가'라는 이름의 국가를 만들어" ," 나라의 주요 행사에 사용했다는 기록은 지금도 남아 있다." });
         talkData.Add(1010, new string[] { "다음대사","AAAAA" });
+        talkData.Add(1011, new string[] { "선택지 1 선택완료", "AAAAA" });
+        talkData.Add(1012, new string[] { "선택지 2 선택완료", "BBBBB" });
+        talkData.Add(1013, new string[] { "선택지 3 선택완료", "CCCCC" });
         talkData.Add(2000, new string[] { "가나다라마바사  아자차카타파하  가나다라마바사  아자차카타파하  가나다라마바사  아자차카타파하" });
+    }
+
+    void OnSelet(int SeletId)
+    {
+        textSelet.onSeletStart();
+
+        if (SeletId == 0)
+        {
+
+        }
+        else if(SeletId == 1)
+        {
+
+        }else if(SeletId == 2)
+        {
+
+        }else
+        {
+
+        }
+        
     }
 
 
