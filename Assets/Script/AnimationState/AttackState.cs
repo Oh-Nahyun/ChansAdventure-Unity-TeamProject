e741ab66c3d9a429b31367418f0d1bd0ffcef163
cwon222 <154861510+cwon222@users.noreply.github.com>
2024-03-20 15:20:51 +0900

    공격 상태 수정

--- Assets/Script/AnimationState/AttackState.cs
+++ Assets/Script/AnimationState/AttackState.cs
@@ -15,8 +15,6 @@ public class AttackState : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform; // 플레이어 태그를 가진 오브젝트 찾기
-        // 사거리 잡아서 거리가 가까워지면 플레이어 바라보게 설정
-        float distance = Vector3.Distance(player.position, animator.transform.position); // 자신과 플레이어의 거리 구하기
         
         
     }
@@ -24,9 +22,12 @@ public class AttackState : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
-
+        // 사거리 잡아서 거리가 가까워지면 플레이어 바라보게 설정
         float distance = Vector3.Distance(player.position, animator.transform.position); // 자신과 플레이어의 거리 구하기
+        if(distance > 0.1f)
+        {
+            animator.transform.LookAt(player);
+        }
         if (distance > endChasingRange) // 자신과 플레이어의 거리가 일정거리 이상이면
         {
             animator.SetBool(isAttacking_Hash, false); // 달리기 애니메이션 설정
