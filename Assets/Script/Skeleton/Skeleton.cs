68f00cf94a7a5bbd0316506757e915ce252801d7
cwon222 <154861510+cwon222@users.noreply.github.com>
2024-03-21 15:16:39 +0900

    스켈레톤 애니메이션 설정
    배회, 추격, 공격, 피격당햇을때, 죽음

---
+++ Assets/Script/Skeleton/Skeleton.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    public float HP = 100.0f;
+
+    public float damage = 20.0f;
+
+    public Action<float> onDamage;
+
+    readonly int die_Hash = Animator.StringToHash("Die");
+    readonly int damage_Hash = Animator.StringToHash("Damage");
+
+    Animator animator;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        HP -= damageAmount;
+        if (HP <= 0)
+        {
+            // 죽는 애니메이션
+            animator.SetTrigger(die_Hash);
+            GetComponent<Collider>().enabled = false;
+        }
+        else
+        {
+            // 맞는 애니메이션
+            animator.SetTrigger(damage_Hash);
+        }
+    }
+
+    public void Test(float damage)
+    {
+        if (Input.GetButton("Space"))
+        {
+            TakeDamage(10.0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onDamage?.Invoke(damage);
+        }
+    }
+}
