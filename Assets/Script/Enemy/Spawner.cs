dffc78c8d8d6b7d3394d3afa9ad9e2b73344a869
cwon222 <154861510+cwon222@users.noreply.github.com>
2024-03-25 15:20:44 +0900

    할일

--- Assets/Script/Enemy/Spawner.cs
+++ Assets/Script/Enemy/Spawner.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     // https://velog.io/@gkswh4860/Unity-%ED%8A%B9%EC%A0%95-%EB%B2%94%EC%9C%84-%EB%82%B4%EC%97%90%EC%84%9C-%EB%9E%9C%EB%8D%A4%ED%95%9C-%EC%9C%84%EC%B9%98%EC%97%90-%EC%98%A4%EB%B8%8C%EC%A0%9D%ED%8A%B8-%EC%8A%A4%ED%8F%B0%ED%95%98%EA%B8%B0
+
+    // 몬스터 마다 공격력 다르게 만들기(일반몹)
+    // 공격에 맞으면 데미지 감소
+    // 죽으면 죽는다고 델리게이트 보내고 죽기
+    // 공격에 닿으면 델리게이트로 알리기
+    // 신호를 받으면 플레이어는 데미지 감소
+    // 중간 보스는 공격마다 데미지 다르게 설정
+    
 }
