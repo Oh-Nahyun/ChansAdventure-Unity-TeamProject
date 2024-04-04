23ef121df6747eafcf6aa90dff961118e77c2587
mob <lsh954325@gmail.com>
2024-04-04 22:53:16 +0900

    Enemy Merge 완료
    일부 씬에 있는 프리팹 missing 됨
    
    해당 작업하는 인원은 프리팹을 다시 만들면됨
    
    -> Core, Player 프리팹 missing은 현재 프로젝트 내에 있는 프리팹 사용하면 됨

--- Assets/Scripts/Core/Factory.cs
+++ Assets/Scripts/Core/Factory.cs
@@ -14,6 +14,8 @@ public class Factory : Singleton<Factory>
     RemoteBombPool remoteBombPool;
     RemoteBombCubePool remoteBombCubePool;
     MagnetCatchPool magnetCatchPool;
+    SwordSkeletonPool swordSkeletonPool;
+    DamageTextPool damageTextPool;
 
     protected override void OnInitialize()
     {
@@ -25,6 +27,11 @@ public class Factory : Singleton<Factory>
         if (remoteBombCubePool != null) remoteBombCubePool.Initialize();
         magnetCatchPool = GetComponentInChildren<MagnetCatchPool>();
         if (magnetCatchPool != null) magnetCatchPool.Initialize();
+        swordSkeletonPool = GetComponentInChildren<SwordSkeletonPool>();
+        if (swordSkeletonPool != null) swordSkeletonPool.Initialize();
+        damageTextPool = GetComponentInChildren<DamageTextPool>();
+        if (damageTextPool != null) damageTextPool.Initialize();
+
     }
 
     /// <summary>
@@ -69,5 +76,46 @@ public class Factory : Singleton<Factory>
     public MagnetCatch GetMagnetCatch(Vector3? position = null, float angle = 0.0f)
     {
         return magnetCatchPool.GetObject(position, angle * Vector3.forward);
+    }
+    /// <summary>
+    /// 검사스켈레톤 하나 가져오는 함수
+    /// </summary>
+    /// <returns>배치된 슬라임 하나</returns>
+    public SwordSkeleton GetEnemy()
+    {
+        return swordSkeletonPool.GetObject();
+    }
+
+    /// <summary>
+    /// 검사스켈레톤 하나를 특정 위치에, 특정 각도로 배치
+    /// </summary>
+    /// <param name="position">배치될 위치</param>
+    /// <param name="angle">배치 될 때의 각도</param>
+    /// <returns>배치된 슬라임 하나</returns>
+    public SwordSkeleton GetEnemy(Vector3 position, float angle = 0.0f)
+    {
+        return swordSkeletonPool.GetObject(position, angle * Vector3.forward);
+    }
+
+    /// <summary>
+    /// 검사스켈레톤 하나를 특정 웨이포인트를 사용하고, 특정 위치에, 특정 각도로 배치
+    /// </summary>
+    /// <param name="index">사용할 웨이포인트의 인덱스</param>
+    /// <param name="position">배치될 위치</param>
+    /// <param name="angle">배치 될 때의 각도</param>
+    /// <returns>배치된 슬라임 하나</returns>
+    public SwordSkeleton GetEnemy(int index, Vector3 position, float angle = 0.0f)
+    {
+        return swordSkeletonPool.GetObject(index, position, angle * Vector3.forward);
+    }
+
+    /// <summary>
+    /// 데미지 텍스트를 생성하는 함수
+    /// </summary>
+    /// <param name="damage">설정될 데미지</param>
+    /// <returns></returns>
+    public GameObject GetDamageText(int damage, Vector3? position)
+    {
+        return damageTextPool.GetObject(damage, position);
     }
 }
