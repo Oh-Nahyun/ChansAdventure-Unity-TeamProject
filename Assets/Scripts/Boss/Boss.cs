46bb99d4549893d6dbf735ac09dc793d47e732fb
mob <lsh954325@gmail.com>
2024-04-13 15:08:12 +0900

    #10 #11 Merge 정리
    Player, skills Merge 정리 및 외부 에셋 폴더 일부 위치 변경 및 미사용 파일 삭제

---
+++ Assets/Scripts/Boss/Boss.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Boss : MonoBehaviour
+{
+    BossInputActions inputActions;
+    Rigidbody rb;
+    ClowAttackArea clowAttackArea;
+
+    float moveFB = 0.0f;
+    float moveLR = 0.0f;
+    float moveSpeed = 5.0f;
+
+    private void Awake()
+    {
+        inputActions = new BossInputActions();
+        rb = GetComponent<Rigidbody>();
+        clowAttackArea = GetComponentInChildren<ClowAttackArea>(true);
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Boss.Enable();
+        inputActions.Boss.Move.performed += OnMove;
+        inputActions.Boss.Move.canceled += OnMove;
+        inputActions.Boss.Clow.performed += OnClow;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Boss.Move.canceled -= OnMove;
+        inputActions.Boss.Move.performed -= OnMove;
+        inputActions.Boss.Clow.performed -= OnClow;
+        inputActions.Boss.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext obj)
+    {
+        SetInput(obj.ReadValue<Vector2>(), !obj.canceled);
+    }
+
+    private void OnClow(InputAction.CallbackContext obj)
+    {
+        clowAttackArea.Activate();
+    }
+
+
+    private void SetInput(Vector2 input, bool isMove)
+    {
+        moveLR = input.x;
+        moveFB = input.y;
+    }
+
+    
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.MovePosition(rb.position + Time.fixedDeltaTime * moveSpeed * moveFB * transform.forward);
+        rb.MovePosition(rb.position + Time.fixedDeltaTime * moveSpeed * moveLR * transform.right);
+    }
+    protected enum BossState
+    {
+        Wait = 0,   // ´ë±â
+        Bite,       // ¹°±â
+        Breath,     // ºê·¹½º
+        Clow,       // ÇÓÄû±â
+        Dead        // »ç¸Á
+    }
+
+    BossState state = BossState.Wait;
+
+    protected BossState State
+    {
+        get => state;
+        set
+        {
+            if(state != value)
+            {
+                state = value;
+                switch(state)
+                {
+                    case BossState.Wait:
+                        break;
+                    case BossState.Bite:
+                        break;
+                    case BossState.Breath:
+                        break;
+                    case BossState.Clow:
+                        break;
+                    case BossState.Dead:
+                        break;
+                }
+            }
+        }
+
+    }
+
+    protected float hp = 10000.0f;
+    public float HP
+    {
+        get => hp;
+        set
+        {
+            hp = value;
+            if(State != BossState.Dead && hp <= 0)
+            {
+                Die();
+            }
+            hp = Mathf.Clamp(hp, 0, MaxHP);
+            onHealthChange?.Invoke(hp / MaxHP);
+        }
+    }
+
+    public float maxHP = 10000.0f;
+    public float MaxHP => maxHP;
+
+    public Action<float> onHealthChange { get; set; }
+
+    void Die()
+    {
+
+    }
+}
