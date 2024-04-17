afc9f117bac49ed39d457c3a91797995a57c80c1
mob954325 <lsh954325@gmail.com>
2024-04-17 15:19:06 +0900

    테스트용 플레이어 추가
    dotLine 테스트용 플레이어 스크립트 추가

--- Assets/Scripts/Map/Test/Test_Map_Player.cs
+++ Assets/Scripts/Map/Test/Test_Map_Player.cs
@@ -8,6 +8,10 @@ public class Test_Map_Player : MonoBehaviour
 {
     PlayerinputActions inputActions;
     public CanvasGroup map_CanvasGroup;
+    public LineRenderer linePrefab;
+
+    public float speed = 5f;
+    Vector3 moveVector = Vector3.zero;
 
 
     void Awake()
@@ -20,15 +24,32 @@ public class Test_Map_Player : MonoBehaviour
         inputActions.Player.Enable();
         inputActions.Player.Open_Map.performed += OnOpenMap;
         inputActions.Player.Open_Map.canceled += OnOpenMap;
+        inputActions.Player.Move.performed += OnMoveInput;
+        inputActions.Player.Move.canceled += OnMoveInput;
     }
 
     void OnDisable()
     {
+        inputActions.Player.Move.canceled -= OnMoveInput;
+        inputActions.Player.Move.performed -= OnMoveInput;
         inputActions.Player.Open_Map.canceled -= OnOpenMap;
         inputActions.Player.Open_Map.performed -= OnOpenMap;
         inputActions.Player.Disable();
         
     }
+
+    void Update()
+    {
+        transform.position += moveVector * speed * Time.deltaTime;
+    }
+
+    private void OnMoveInput(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>();
+
+        moveVector = new Vector3(inputVector.x, 0, inputVector.y);
+    }
+
     private void OnOpenMap(InputAction.CallbackContext obj)
     {
         // 임시 온오프
