bdc1e907a666dc1e9f57f4dcc1df50199effaea1
uk <134572636+bide879@users.noreply.github.com>
2024-04-22 15:21:03 +0900

    ShopPanel에서 ShopItemData에게 데이터 전달 완료
    ShopPanel에 마우스를 올렸을 때 해당 ShopPanel의 ItemData를 ShopItemData에서 보여주기 완료

--- Assets/Scripts/Data/Dialog/Shop/ShopItemData.cs
+++ Assets/Scripts/Data/Dialog/Shop/ShopItemData.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopItemData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    ItemData itemData;
+
+    Image itemImage;
+    TextMeshProUGUI itemNameText;
+    TextMeshProUGUI itemDataText;
+    TextMeshProUGUI itemPriceText;
+
+    private void Awake()
     {
-        
+        Transform child = transform.GetChild(1);
+        itemImage = child.GetComponent<Image>();
+
+        child = transform.GetChild(2);
+        itemNameText = child.GetComponent<TextMeshProUGUI>();
+
+        child = transform.GetChild(3);
+        itemDataText = child.GetComponent<TextMeshProUGUI>();
+
+        child = transform.GetChild(4);
+        itemPriceText = child.GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (itemData != null)
+        {
+            itemImage.sprite = itemData.itemIcon;
+            itemNameText.text = itemData.itemName;
+            itemDataText.text = itemData.desc;
+            itemPriceText.text = itemData.price.ToString();
+        }
+    }
+
+    public void GetItemData(ItemData newItemData)
+    {
+        itemData = newItemData;
+        gameObject.SetActive(true);
     }
 }
--- Assets/Scripts/Data/Dialog/Shop/ShopItemPanel.cs
+++ Assets/Scripts/Data/Dialog/Shop/ShopItemPanel.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ShopItemPanel : MonoBehaviour
+public class ShopItemPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     Image itemImage;
     TextMeshProUGUI itemNameText;
@@ -12,7 +13,11 @@ public class ShopItemPanel : MonoBehaviour
     TextMeshProUGUI itemPriceText;
 
     public ItemData itemData;
+    ShopItemData shopItemData;
 
+    /// <summary>
+    /// 아이템의 남은 재고
+    /// </summary>
     public int itemStock = 10;
 
     private void Awake()
@@ -29,6 +34,7 @@ public class ShopItemPanel : MonoBehaviour
         child = transform.GetChild(3);
         child = child.transform.GetChild(1);
         itemPriceText = child.GetComponent<TextMeshProUGUI>();
+        shopItemData = FindAnyObjectByType<ShopItemData>();
     }
 
     private void Start()
@@ -42,5 +48,36 @@ public class ShopItemPanel : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 마우스가 들어왔을 때 실행하는 함수
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("마우스 들어옴");
+        if (shopItemData != null)
+        {
+            // 해당 패널의 ItemData를 건내줌
+            shopItemData.GetItemData(itemData);
+        }
+    }
 
+    /// <summary>
+    /// 마우스가 나갔을 때 실행하는 함수
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("마우스 나감");
+        if (shopItemData != null)
+        {
+            shopItemData.gameObject.SetActive(false);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("마우스 클릭");
+        
+    }
 }
