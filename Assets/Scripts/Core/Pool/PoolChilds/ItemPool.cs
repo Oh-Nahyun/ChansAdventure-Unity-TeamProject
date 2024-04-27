295141a369a6ea10e1b471c1a964d1c7b9b910b3
mob <lsh954325@gmail.com>
2024-04-27 16:28:34 +0900

    ItemPool 임시 함수 제거

--- Assets/Scripts/Core/Pool/PoolChilds/ItemPool.cs
+++ Assets/Scripts/Core/Pool/PoolChilds/ItemPool.cs
@@ -21,18 +21,4 @@ public class ItemPool : ObjectPool<ItemDataObject>
 
         return parentObj.gameObject;                                            // Factory�� ������Ʈ ��ȯ ( �������� �ڽ� 0��° )
     }
-
-    // 임시로 함수 추가(나중에 뺴야딤) -------------------------------------------------------------------
-    public GameObject GetItemObject(ItemData data, Vector3? position = null)
-    {
-        GameObject itemObj = data.ItemPrefab;                      // ������ ������ ����
-
-        ItemDataObject parentObj = GetObject(position);                         // Ǯ���� ������ ������
-        parentObj.GetComponent<ItemDataObject>().SetData(data);    // ���� �������� ������ ������ ����
-
-        Instantiate(itemObj, parentObj.transform);                              // ������ ������ ������ ����
-
-        return parentObj.gameObject;                                            // Factory�� ������Ʈ ��ȯ ( �������� �ڽ� 0��° )
-    }
-    // ------------------------------------------------------------------------------------------
 }
