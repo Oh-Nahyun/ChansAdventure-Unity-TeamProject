eeeb4b892db1e1bf806764b2c04b22d15d482fd5
bide879 <bide879@gmail.com>
2024-04-29 00:55:47 +0900

    상점 판매 추가중

--- Assets/Scripts/Data/Dialog/Shop/SaleInfo.cs
+++ Assets/Scripts/Data/Dialog/Shop/SaleInfo.cs
@@ -1,32 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
 
 public class SaleInfo : MonoBehaviour
 {
     InventoryUI inventoryUI;
-
+    Inventory inven;
     /// <summary>
     /// UI slots
     /// </summary>
-    InventorySlotUI[] slotsUIs;
+    SaleInfoSolt[] saleInfoSolts;
 
     private void Awake()
     {
         inventoryUI = FindAnyObjectByType<InventoryUI>();
+        inven = inventoryUI.Inventory;
     }
 
     private void Start()
     {
-       
+        InitializeInventoryUI(inven);
     }
+
     public void InitializeInventoryUI(Inventory playerInventory)
     {
-        slotsUIs = GetComponentsInChildren<InventorySlotUI>();  // 일반 슬롯
-
+        saleInfoSolts = GetComponentsInChildren<SaleInfoSolt>();  // 일반 슬롯
         for (uint i = 0; i < inventoryUI.Inventory.SlotSize; i++)
         {
-            slotsUIs[i].InitializeSlotUI(inventoryUI.Inventory[i]); // 인벤토리슬롯을 slotUI와 연결
+            saleInfoSolts[i].InitializeSlotUI(inventoryUI.Inventory[i]); // 인벤토리슬롯을 slotUI와 연결
         }
     }
 }
---
+++ Assets/Scripts/Data/Dialog/Shop/SaleInfoSolt.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaleInfoSolt : SlotUI_Base
+{
+
+    public InventorySlotUI inventorySlotUI;
+    InventoryUI inventoryUI;
+
+    private void Start()
+    {
+        
+    }
+
+
+}
